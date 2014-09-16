f3df8fa9a4944d5a7eea0756791692b74f897a09
Chad McElwain <chadmcelwain@Jordans-MacBook-Pro.local>
2014-09-16 02:13:37 -0500

    Settings Page
    Initial layout and fundamental ui. Swipe motions added to search and
    settings page respectively from meet screen.

---
+++ REON/ReonSettings.h
@@ -0,0 +1,13 @@
+//
+//  ReonSettings.h
+//  REON
+//
+//  Created by Chad McElwain on 9/16/14.
+//  Copyright (c) 2014 OWWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReonSettings : UIViewController
+
+@end
---
+++ REON/ReonSettings.m
@@ -0,0 +1,49 @@
+//
+//  ReonSettings.m
+//  REON
+//
+//  Created by Chad McElwain on 9/16/14.
+//  Copyright (c) 2014 OWWS. All rights reserved.
+//
+
+#import "ReonSettings.h"
+
+@interface ReonSettings ()
+
+@end
+
+@implementation ReonSettings
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
