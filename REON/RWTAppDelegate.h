1af5e5d11cd6fe9201b598e8d9743e1199d6e526
Chad McElwain <chadmcelwain@Jordans-MacBook-Pro.local>
2014-09-18 09:14:06 -0500

    corelocation wakeup
    Adding method for waking up phone.

---
+++ REON/RWTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWTAppDelegate.h
+//  REON
+//
+//  Created by Chad McElwain on 9/17/14.
+//  Copyright (c) 2014 Remember Everyone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
